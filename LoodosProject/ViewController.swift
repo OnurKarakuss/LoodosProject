dd05bf201027f088bf77ec7539e418f68b340fbc
Onur Karakuş <onurkarakus9562@gmail.com>
2019-08-17 22:45:09 +0300

    Initial Commit

---
+++ LoodosProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoodosProject
+//
+//  Created by Onur Karakuş on 17.08.2019.
+//  Copyright © 2019 Onur Karakuş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
